d303ebd23d8699197a25cde65e593b7ddd354dab
Bekboosun Abdyldaev <beks.repo@gmail.com>
2023-06-27 16:13:18 +0600

    Add import

--- android/src/main/java/kg/storekey/secure_key/SecureKeyPlugin.java
+++ android/src/main/java/kg/storekey/secure_key/SecureKeyPlugin.java
@@ -2,6 +2,8 @@ package kg.storekey.secure_key;
 
 import android.content.Context;
 import androidx.annotation.NonNull;
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.Result;
